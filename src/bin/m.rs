a71960842811e7634d488a535f392404904bb26c
Aaron Loucks <aloucks@cofront.net>
2020-09-06 10:14:20 -0400

    Move docs helper to workspace member

--- src/bin/m.rs
+++
@@ -1,9 +0,0 @@
-use directx_math::*;
-
-fn main() {
-    let m = XMMatrixTranslation(2.0, 3.0, 4.0);
-    // let mut array: [[f32; 4]; 4] = Default::default();
-    let mut array = XMFLOAT4X4::default();
-    XMStoreFloat4x4(&mut array, m);
-    println!("{:?}", array);
-}
